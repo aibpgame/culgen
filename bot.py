80da1c5bab7d5e6e1d47b025d0fa56d6333309e1
aibpgame <76448681+aibpgame@users.noreply.github.com>
2021-06-20 14:24:30 +0900

    .
    .

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+import random
+ 
+client = discord.Client()
+ 
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+ 
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        channel = message.channel
+        num = random.randrange(1000, 9999)
+        num1 = random.randrange(1000, 9999)
+        num2 = random.randrange(1000, 9999)
+        num3 = random.randrange(100000, 999999)
+        num4 = "-"
+        msg = ('{0} {1} {2} {3} {4} {5} {6}'.format(num, num4, num1, num4, num2, num4, num3))
+        await channel.send(msg)
+        return None
+ 
+client.run('ODU2MDMxOTY5OTYzMjc4Mzk2.YM7H5Q.1seBmcyUbB9r4B22t5TfGJHX1L8')
